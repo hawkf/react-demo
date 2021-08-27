f2f18d1364bbce620c3886e9a8d6390d6dbd05b4
hawkf <fedor.gorokhov@gmail.com>
2021-08-27 12:19:31 +0300

    Исправил ошибку с добавлением лого компании

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,15 +1,11 @@
 import React from "react";
+import logo from "./logoCompany.png";
 
 export function Header() {
   return (
     <header className='page-header'>
       <div>
-        <img
-          src='./img/logoCompany.png'
-          height='40px'
-          width='350px'
-          alt='Логотип компании'
-        />
+        <img src={logo} height='40px' width='350px' alt='Логотип компании' />
       </div>
       <p className='page-header__history'>
         Чверть століття тому було покладено початок великого шляху. Сьогодні ми
