2b09adf121b33bfca878e520446cda4046bfaa70
hawkf <fedor.gorokhov@gmail.com>
2021-08-30 15:01:49 +0300

    Сделал проверку введенных баллов в одном вопросе

--- src/form.jsx
+++ src/form.jsx
@@ -1,24 +1,53 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
 import swal from "sweetalert";
 
 export function Form(props) {
   window.scrollTo(0, 0);
   const { questionElements, changeQuestionHandler, bitrixFields } = props;
 
-  const question1 = useRef();
-  const question2 = useRef();
-  const question3 = useRef();
-  const question4 = useRef();
+  const [question1, setQuestion1] = useState("");
+  const [question2, setQuestion2] = useState("");
+  const [question3, setQuestion3] = useState("");
+  const [question4, setQuestion4] = useState("");
 
   const questionRefs = [question1, question2, question3, question4];
+  const setQuestions = [setQuestion1, setQuestion2, setQuestion3, setQuestion4];
+
+  const onChangeHandler = (evt, name) => {
+    if (evt.target.value < 0) {
+      swal("Количество баллов не может быть меньше 0");
+      return;
+    }
+
+    if (evt.target.value > 11) {
+      swal("Количество баллов не может быть больше 11");
+      return;
+    }
+    switch (name) {
+      case "1":
+        setQuestion1(evt.target.value);
+        break;
+      case "2":
+        setQuestion2(evt.target.value);
+        break;
+      case "3":
+        setQuestion3(evt.target.value);
+        break;
+      case "4":
+        setQuestion4(evt.target.value);
+        break;
+      default:
+        return;
+    }
+  };
 
   const onSubmitHandler = (evt) => {
     evt.preventDefault();
     let defaultSum = 0;
 
-    questionRefs.forEach((ref) => {
-      if (ref.current.value !== "" && !isNaN(ref.current.value)) {
-        defaultSum = defaultSum + parseFloat(ref.current.value);
+    questionRefs.forEach((item) => {
+      if (item !== "" && !isNaN(item)) {
+        defaultSum = defaultSum + parseFloat(item);
       }
     });
 
@@ -27,15 +56,11 @@ export function Form(props) {
       return;
     }
 
-    let values = questionRefs.map((item) => {
-      return item.current.value;
+    setQuestions.forEach((item) => {
+      item("");
     });
 
-    questionRefs.forEach((ref) => {
-      ref.current.value = "";
-    });
-
-    changeQuestionHandler(bitrixFields, values);
+    changeQuestionHandler(bitrixFields, questionRefs);
   };
 
   return (
@@ -49,10 +74,13 @@ export function Form(props) {
             >{`Ответ №${questionName}`}</label>
             <div className='qustion-form__input-container'>
               <input
-                ref={questionRefs[index]}
+                value={questionRefs[index]}
+                onChange={(evt) => onChangeHandler(evt, questionName)}
                 placeholder='0'
                 className='qustion-form__input'
                 type='number'
+                min='0'
+                max='11'
                 id={questionName}
               />
               <p>Баллов</p>
